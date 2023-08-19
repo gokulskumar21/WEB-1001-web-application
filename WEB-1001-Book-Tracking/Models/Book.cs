99a77b579bbb7ea36e00e881772b1a82ea6db807
gokulskumar21 <132939596+gokulskumar21@users.noreply.github.com>
2023-08-18 19:40:55 -0700

    Added  data models as per ERD

---
+++ WEB-1001-Book-Tracking/Models/Book.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using schema = Schema.NET;
+
+namespace WEB_1001_Book_Tracking.Models
+{
+    public class Book
+    {
+        [Key]
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [ForeignKey("Category")]
+        [Display(Name = "Category")]
+        [Required]
+        public string CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public schema.Book GetJson()
+        {
+            schema.Book book = new schema.Book();
+
+            book.Isbn = this.ISBN;
+            book.Name = this.Title;
+            book.Author = new schema.Person() { Name = this.Author };
+            book.About = new schema.CreativeWork()
+                {
+                    Genre = this.Category.NameToken,
+                    IsPartOf = new schema.CreativeWork() { Genre = this.Category.Type.Name }
+                };
+
+            return book;
+        }
+    }
+}
---
+++ WEB-1001-Book-Tracking/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WEB_1001_Book_Tracking.Data;
+using local = WEB_1001_Book_Tracking.Models;
+using schema = Schema.NET;
+
+namespace WEB_1001_Book_Tracking.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WEB_1001_Book_Tracking.Data.BookTrackingDbContext _context;
+
+        public IndexModel(WEB_1001_Book_Tracking.Data.BookTrackingDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<schema.Thing> JSONLD { get; set; }
+
+        public IList<local.Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Books
+                .Include(b => b.Category)
+                .ThenInclude(c => c.Type)
+                .ToListAsync();
+
+            List<schema.Thing> list = new List<schema.Thing>();
+            foreach (var book in Book)
+            {
+                list.Add(book.GetJson());
+            }
+
+            JSONLD = list;
+        }
+    }
+}
